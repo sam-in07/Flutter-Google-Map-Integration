f46c60953cae13892124381d213b10f169fead3a
Samin Ahmed Chowdhury <85004796+sam-in07@users.noreply.github.com>
2025-10-05 13:44:20 +0600

    "android setup done"

---
+++ lib/MapsBasicPage.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Mapsbasicpage extends StatefulWidget {
+  const Mapsbasicpage({super.key});
+
+  @override
+  State<Mapsbasicpage> createState() => _MapsbasicpageState();
+}
+
+class _MapsbasicpageState extends State<Mapsbasicpage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
